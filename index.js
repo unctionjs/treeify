282db5178f45b2497f88fd7865ad0e4b3da6cee3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-import thrush from "@unction/thrush"
-import reduceValues from "@unction/reducevalues"
-import mapValues from "@unction/mapvalues"
-import mapValuesWithValueKey from "@unction/mapvalueswithvaluekey"
-import nestedApply from "@unction/nestedapply"
-
-export default function treeify (folders: Array<(any => FunctorType => FunctorType)>): Function {
-  const [initial, ...remaining] = folders
-
-  return function treeifyIterators (records: Array<FunctorType>): FunctorType {
-    return reduceValues(
-      thrush
-    )(
-      initial(records)
-    )(
-      mapValuesWithValueKey(
-        nestedApply(mapValues)
-      )(
-        remaining
-      )
-    )
-  }
+import thrush from "@unction/thrush";
+import reduceValues from "@unction/reducevalues";
+import mapValues from "@unction/mapvalues";
+import mapValuesWithValueKey from "@unction/mapvalueswithvaluekey";
+import nestedApply from "@unction/nestedapply";
+export default function treeify(folders) {
+  const [initial, ...remaining] = folders;
+  return function treeifyIterators(records) {
+    return reduceValues(thrush)(initial(records))(mapValuesWithValueKey(nestedApply(mapValues))(remaining));
+  };
 }
